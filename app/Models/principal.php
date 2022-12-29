33360e3f1e866b1caac36e9d068e247ff7932a24
leodan12 <leomacip4@gmail.com>
2022-12-29 15:38:04 -0500

    carrusel principal listo

--- app/Models/principal.php
+++ app/Models/principal.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class principal extends Model
+class Principal extends Model
 {
     use HasFactory;
 }
